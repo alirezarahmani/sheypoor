f8d36635fa3e7d73ae9519912de60e95f545aa90
alireza Rahmani khalili <sanfrancisco.alireza@gmail.com>
2017-08-26 10:32:07 +0430

    add some test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,19 +2,59 @@
 
 namespace Tests\Unit;
 
+use Backend\domain\model\aggregate\motorbike;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+
+    public function index_gateway_should_return_jsonTest()
     {
-        $this->assertTrue(true);
+        $this->get('admin/index')
+            ->seeJson();
     }
+
+    public function show_gateway_should_return_jsonTest()
+    {
+        $this->get('admin/show')
+            ->seeJson();
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function must_return_exceptionTest()
+    {
+       $mb = new  motorbike(0);
+       $mb->create();
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function must_return_exception_wrong_weightTest()
+    {
+        $mb = new  motorbike(0);
+        $mb->create(['price'=>120,'model'=>'1500-metal','color'=>'blue','weight'=>10]);
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function must_return_exception_wrong_modelTest()
+    {
+        $mb = new  motorbike(0);
+        $mb->create(['price'=>120,'model'=>'150etal','color'=>'blue','weight'=>10]);
+    }
+
+
+    public function must_return_trueTest()
+    {
+       $mb = new  motorbike(0);
+       $this->assertTrue($mb->create(['price'=>120,'model'=>'1500-metal','color'=>'blue','weight'=>10000]));
+    }
+
+
 }
